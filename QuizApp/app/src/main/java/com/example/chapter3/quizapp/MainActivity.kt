a99880d1dd9c47c8dac7e3db5cc36754193f8edf
nur-magomed <nur-magomed@mail.ru>
2024-08-12 22:33:52 +0300

    questions

--- QuizApp/app/src/main/java/com/example/chapter3/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/chapter3/quizapp/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.chapter3.quizapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.SyncStateContract
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.chapter3.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        val buttonStart = binding.btnStart
+        val nameTxt = binding.etName
+        buttonStart.setOnClickListener {
+            if (nameTxt.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
+                intent.putExtra(SyncStateContract.Constants.ACCOUNT_NAME, nameTxt.text.toString())
+                startActivity(intent)
+                finish()
+            }
         }
+
     }
 }
